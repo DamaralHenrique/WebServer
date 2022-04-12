55c09a54b826f6e723fe276b95c6454a0d109cd3
DamaralHenrique <rique01101999@gmail.com>
2022-04-12 19:42:36 -0300

    🚧 Add aula 5 exercise

---
+++ src/routes/imports.js
@@ -0,0 +1,4 @@
+export { default as productRouter } from './products.js';
+export { default as streamsRouter } from './streams.js';
+// export { default as channelsRouter } from './channels';
+// export { default as gamesRouter } from './games';
